19bfe9a83d9df0f7881da4723eea0447b2617268
F3LuxRay <39596664+F3LuxRay@users.noreply.github.com>
2018-10-23 12:13:29 -0400

    Add files via upload

---
+++ DIGITALTOUCH.ino
@@ -0,0 +1,31 @@
+/*
+Arduino     Touch Sensor
+  5V         VCC
+  GND        GND
+  D3         SIG
+*/
+
+int TouchSensor = 3; //connected to Digital pin D3
+int onLed = 6;
+int offLed = 13;
+void setup(){
+  Serial.begin(9600); // Communication speed
+  pinMode(onLed, OUTPUT);
+  pinMode(offLed, OUTPUT);
+  pinMode(TouchSensor, INPUT);
+}
+
+void loop(){
+  if(digitalRead(TouchSensor)==HIGH)       //Read Touch sensor signal
+   { 
+    digitalWrite(onLed, HIGH);   // if Touch sensor is HIGH, then turn on the onLed
+    digitalWrite(offLed, LOW);    // if Touch sensor is LOW, then turn off the offLed
+    Serial.println("Led ON");
+   }
+  else
+   {
+    digitalWrite(onLed, LOW);    // if Touch sensor is LOW, then turn off the onLed
+    digitalWrite(offLed, HIGH);    // if Touch sensor is LOW, then turn on the offLed
+    Serial.println("Led OFF");
+   }
+}
